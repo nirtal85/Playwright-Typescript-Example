752c326a3932c7bec95c01e1f850160f69e2da53
Nir.Tal <Nir.Tal@aradtec.com>
2024-07-21 16:18:05 +0300

    updates

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,9 +1,11 @@
 module.exports = {
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/stylistic',
-    'prettier',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript"
   ],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
