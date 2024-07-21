d17f8dde18da67a82d15b66bde5fb2b6bd00542e
Nir.Tal <Nir.Tal@aradtec.com>
2024-07-21 14:16:22 +0300

    WIP

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -26,8 +26,9 @@ test(
     await page.goto("https://playwright.dev/");
 
     // Click the get started link.
-    await page.getByRole("link", { name: "Get started" }).click({ force: true });
+    await page.getByRole("link", { name: "Get started" }).click();
 
     // Expects page to have a heading with the name of Installation.
+    await expect(page.getByRole("heading", { name: "Installation" })).toBeVisible();
   }
 );
