17d44c458b5728c98106a455082d464bce2b631c
Nir.Tal <Nir.Tal@aradtec.com>
2024-08-24 02:00:21 +0300

    env

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,8 +4,9 @@ import { defineConfig } from '@playwright/test';
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-// import dotenv from 'dotenv';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
+import { config as dotenvConfig } from 'dotenv';
+
+dotenvConfig();
 
 /**
  * See https://playwright.dev/docs/test-configuration.
@@ -25,7 +26,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
+    baseURL: process.env.BASE_URL,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     screenshot: 'only-on-failure',
