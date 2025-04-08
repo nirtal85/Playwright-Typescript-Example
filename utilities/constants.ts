939d28b65dd72961a77a126b57817fc7b44efdfd
nirtal2 <nir.tal2@talkspace.com>
2025-04-08 11:16:36 +0300

    resources

--- utilities/constants.ts
+++ utilities/constants.ts
@@ -7,5 +7,5 @@ const __dirname = dirname(__filename);
 export class Constants {
   static readonly DIFF_TOLERANCE_PERCENT: number = 0.01;
   static readonly AUTOMATION_USER_AGENT: string = 'automation';
-  static readonly DATA_PATH: string = join(__dirname, '..', 'data');
+  static readonly DATA_PATH: string = join(__dirname, '..', 'resources');
 }
